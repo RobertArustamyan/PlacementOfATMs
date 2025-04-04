807e95c7655ece2cad52b908e7bfd9e38e215913
RobertArustamyan <robertarustamyan2@gmail.com>
2025-04-04 12:28:27 +0200

    Created generate_user_coordinates.py which is used for creating users data in selected range of the map
    Signed-off-by: RobertArustamyan <robertarustamyan2@gmail.com>

---
+++ utils/generate_user_coordinates.py
@@ -0,0 +1,45 @@
+import math
+from models.user import User
+from utils.users_living_quadrilateral_reading import get_living_quadrilateral_data
+from typing import List, Tuple
+import random
+
+
+def generate_users_in_triangle(coordinates: List[Tuple[float, float]]) -> User:
+    random_number_1 = random.random()
+    random_number_2 = random.random()
+
+    if random_number_1 + random_number_2 > 1:
+        random_number_1, random_number_2 = 1 - random_number_1, 1 - random_number_2
+
+    user_latitude = (1 - random_number_1 - random_number_2) * coordinates[0][0] + random_number_1 * coordinates[1][
+        0] + random_number_2 * coordinates[2][0]
+    user_longitude = (1 - random_number_1 - random_number_2) * coordinates[0][1] + random_number_1 * coordinates[1][
+        1] + random_number_2 * coordinates[2][1]
+
+    return User(latitude=round(user_latitude, 7), longitude=round(user_longitude, 7), maxDistance=0)
+
+
+def generate_users_in_quadrilateral(quadrilateral_coordinates: List[Tuple[float, float]], number: int):
+    users = []
+
+    for i in range(number // 2):
+        ordered_points = order_points(quadrilateral_coordinates)
+        users.append(generate_users_in_triangle([ordered_points[0], ordered_points[1], ordered_points[2]]))
+        users.append(generate_users_in_triangle([ordered_points[0], ordered_points[2], ordered_points[3]]))
+    return users
+
+def order_points(coordinates):
+    Cx = sum(coordinate[0] for coordinate in coordinates) / 4
+    Cy = sum(coordinate[1] for coordinate in coordinates) / 4
+
+    points = coordinates.copy()
+    points.sort(key=lambda p: math.atan2(p[1] - Cy, p[0] - Cx))
+    return points
+
+if __name__ == '__main__':
+    data = get_living_quadrilateral_data(
+        "C:/Users/User/PycharmProjects/PlacementOfATMs/ATMsData/LeobenUserLivingSquares.csv")
+    # print(generate_users_in_quadrilateral(data[1],10))
+    # print(order_points(data[1]))
+    print(generate_users_in_quadrilateral(data[1], 1000))
